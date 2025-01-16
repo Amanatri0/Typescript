b16a7c10a133beecbeb4adc18df9d476b699c071
Amanatri0 <atriaman8897@gmail.com>
2025-01-16 09:46:32 +0530

    Seperated folders

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function delayedCall(anotherFn) {
-    setTimeout(() => anotherFn("Aman"), 2000);
-}
-function greet(name) {
-    console.log(`Hello ${name}`);
-}
-delayedCall(greet);
